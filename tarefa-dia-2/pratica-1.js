bc7f74f7bf18332ade6f5e50b64dcfefe1baa16e
Larissa Sardinha <larissaribeirosardinha@gmail.com>
2022-08-27 11:25:59 -0300

    Template STrings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -5,3 +5,5 @@ let idade = 22;
 let lugar_dos_sonhos = "Londres";
 
 console.log(nome, email, profissao, idade, lugar_dos_sonhos);
+
+console.log("Olá " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + lugar_dos_sonhos+ ". Confirma pra gente alguns dados? A sua idade é " +idade+ ", você é " +profissao+ " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D");
