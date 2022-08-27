bbac0fdb36399e012ed472702e8e6d64443ce9b4
Larissa Sardinha <larissaribeirosardinha@gmail.com>
2022-08-27 11:18:16 -0300

    Exibindo variáveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,5 @@ let email = "larissasardinha@icloud.com";
 let profissao = "auxiliar de fotografia";
 let idade = 22;
 let lugar_dos_sonhos = "Londres";
+
+console.log(nome, email, profissao, idade, lugar_dos_sonhos);
